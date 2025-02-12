702a11fd4e4b7e03ffd063e4e9bd3c8c6fc563ef
mudit-dixit <muditdixit99@gmail.com>
2025-02-12 16:56:54 +0530

    fixed url

--- src/components/chats.jsx
+++ src/components/chats.jsx
@@ -17,7 +17,7 @@ const Chat = () => {
 
     try {
       const response = await axios.post(
-        'http://http://13.235.80.128/:5000/api/chat/ask',
+        'http://http://13.235.80.128:5000/api/chat/ask',
         {
           question: input,
         }
